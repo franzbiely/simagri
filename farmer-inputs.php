aa229f922c5f7a8c48466cf72c19cd2056abe41b
Francis Albores <mrthemetribe@gmail.com>
2018-08-25 12:22:07 +0800

    Farmer input

--- farmer-inputs.php
+++ farmer-inputs.php
@@ -1,38 +1,24 @@
 <link rel="stylesheet" href="./css/global.css" />
 <link rel="stylesheet" href="./css/layout.css" />
+<link rel="stylesheet" href="./css/inputs.css" />
 <link rel="stylesheet" href="./css/progress.css" />
-<style>
-	.index {
-		background: url(./img/splash.jpg) center;
-		background-size: cover;
-		width:100%;
-		height: 100%;
-		position: fixed;
-	}
-	button {
-		margin-top: 100px;
-	}
-	h1 {
-		font-size: 150px;
-		margin-bottom: 0;
-		text-shadow: 2px 2px 1px #fff;
-	}
-	p {
-		text-shadow: 1px 1px 0px #fff;
-	}
-</style>
 <body>
-	<div class="index flex-container">
+	<div class="farmer-inputs flex-container">
 		<div class="row">
-			<h1>Sims Agri</h1>
-			<p>Developed by Artificers</p>
+			<h3>Hey Farmer!</h3>
+			<p> Please choose your farm location.</p>
+			<select >
+				<option>[Please select]</option>
+				<option value='tugbok-davao'>Tugbok Davao</option>
+				<option value='manambulan-davao'>Manambulan Davao</option>
+			</select>
+			<br />
 			<button>Enter</button>
 		</div>
 	</div>
 </body>
 <script src="./js/jquery.js"></script>
 <script src="./js/progress.js"></script>
-<script src="./js/phase.js"></script>
 <script>
 $('button').on('click',function() {
 	var tasks = {
@@ -43,7 +29,7 @@ $('button').on('click',function() {
 			}
 		},
 		2 : {
-			title : 'Rendering',
+			title : 'Rendering Maps',
 			func : function(){
 				console.log('Rendering');
 			}
@@ -53,15 +39,11 @@ $('button').on('click',function() {
 			func : function() {
 				setTimeout(function() {
 					Phase.out()
-					window.location.href='./farmer-inputs.php';
-				},1000)			
+					window.location.href='./maps.php';
+				},500)			
 			}
 		}
 	}
 	ProgressBar.render(tasks)
-	
-	// window.history.pushState("", "", '/farmer-details');
-
-
 })
 </script>
