1fd96c2fe67122244691f82cbf717f7bcbac55b4
Francis Albores <mrthemetribe@gmail.com>
2018-08-25 11:37:44 +0800

    Splash screen init

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
 <link rel="stylesheet" href="./css/global.css" />
+<link rel="stylesheet" href="./css/layout.css" />
+<style>
+	.index {
+		background: url(./img/splash.jpg) center;
+		background-size: cover;
+		width:100%;
+		height: 100%;
+		position: fixed;
+	}
+	button {
+		margin-top: 100px;
+	}
+	h1 {
+		font-size: 150px;
+		margin-bottom: 0;
+		text-shadow: 2px 2px 1px #fff;
+	}
+	p {
+		text-shadow: 1px 1px 0px #fff;
+	}
+</style>
 <body>
-	<div class="index">
-		
+	<div class="index flex-container">
+		<div class="row">
+			<h1>Sims Agri</h1>
+			<p>Developed by Artificers</p>
+			<button>Enter</button>
+		</div>
 	</div>
 </body>
 <script src="./js/jquery.js"></script>
